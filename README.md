2b6f301321d112c681a092eb51ef63d3267fe86b
Michael Patricios <michael.patricios@gmail.com>
2015-07-13 16:16:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## React Native Tutorial
+
+From: http://www.raywenderlich.com/99473/introducing-react-native-building-apps-javascript
---
+++ index.ios.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+} = React;
+
+var SearchPage = require('./SearchPage');
+
+var styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+class PropertyFinder extends React.Component {
+  render() {
+    return (
+      <React.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
